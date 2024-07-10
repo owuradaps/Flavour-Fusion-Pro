9a2736f3897062f17ccfa25cdea332ce49b26d15
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-10 12:03:21 +0000

    Add Password reset

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -152,6 +152,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
