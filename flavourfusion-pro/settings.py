fa2908fc1a962512de0616e5e1efde8fd908b2cb
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-12 10:04:13 +0000

    move media folder to static folder

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # For Heroku deployment
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'static/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
