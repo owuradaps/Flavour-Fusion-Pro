835debf307103037e97d0c60792cec1ca0d47106
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-15 13:07:13 +0000

    debuging settings file

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = "flavourfusion-pro.wsgi.application"
 # }
 
 # Database
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 CSRF_TRUSTED_ORIGINS = [
