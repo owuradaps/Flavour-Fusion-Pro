b467309030173cc83d339f8f39885fb51061466c
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-15 13:11:37 +0000

    point images upload to static folger

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 
 
 if not DEBUG:
