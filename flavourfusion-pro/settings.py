d29e0f3c9321e3e693512e14a4c5a710673a8d4d
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-15 21:29:44 +0000

    update settings to capture deployment logs

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -151,6 +151,23 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
 # For Heroku deployment
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
