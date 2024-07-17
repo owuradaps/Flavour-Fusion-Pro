177d99aa6dd1915da4fd4665a1d086b971528262
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-17 16:04:28 +0000

    Update settings for environment variables

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -208,9 +208,3 @@ CLOUDINARY_STORAGE = {
 
 # MEDIA_URL = "/media/"
 # DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
-# CLOUDINARY_STORAGE = {
-#     "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
-#     "API_KEY": config("CLOUDINARY_API_KEY"),
-#     "API_SECRET": config("CLOUDINARY_API_SECRET"),
-# }
