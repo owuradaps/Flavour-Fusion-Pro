0e6b4be419546a8f1c471e18eebc43f7d17dafc7
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-17 16:48:31 +0000

    Updade settings.py for deployment

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "cloudinary_storage",
     "cloudinary",
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 # Middleware
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
