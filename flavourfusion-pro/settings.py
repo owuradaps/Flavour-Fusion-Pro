1265fb5120adde362bd1df2ba46dd11cd4019d25
Ernest Dapaah <ernestdaps@yahoo.com>
2024-07-19 10:36:51 +0000

    Update settings.py for deployment

--- flavourfusion-pro/settings.py
+++ flavourfusion-pro/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Debug mode
-DEBUG = False  # os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # Allowed hosts
 ALLOWED_HOSTS = [
